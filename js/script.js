9f5020b0a5b1cc814fc2f8c8343ebe8ba1d8f587
yuzo <112663748+yuzowi@users.noreply.github.com>
2023-09-06 01:24:46 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,40 @@
+const headerEl = document.querySelector('.header');
+
+window.addEventListener('scroll', () =>{
+    if(window.scrollY > 100){
+        headerEl.classList.add('header-scrolled');
+    }else if(window.scrollY <= 100){
+        headerEl.classList.remove('header-scrolled');
+    }
+});
+
+const player = videojs('header-video');
+player.controlBar.progressControl.hide();
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
